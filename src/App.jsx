c2eaa7caa3886e008e41599e8a7b689d215f8caf
Hilal38 <hhilalkocak@gmail.com>
2024-06-07 23:37:00 +0300

    Generate questions based on API

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [flashCards, setFlashCards] = useState([])
   const [categories, setCategories] = useState([])
   const categoryEl = useRef()
+  const amountEl = useRef()
 
   useEffect(() => {
     axios
@@ -45,6 +46,27 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault()
+
+    axios
+    .get('https://opentdb.com/api.php?amount=10', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+      setFlashCards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [...questionItem.incorrect_answers.
+          map(a => decodeString(a)), answer]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
   }
 
   return (
@@ -59,6 +81,15 @@ function App() {
             </option>
           })}
         </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="amount">Number of Questions</label>
+        <input type="number" id="amount" min="1" defaultValue={10} ref={amountEl} />
+
+      </div>
+
+      <div className="form-group">
+        <button className='btn'>Generate</button>
       </div>
 
     </form>
